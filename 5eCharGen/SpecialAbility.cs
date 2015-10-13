008211a807cb4e6acef273beba3f9009d8e0b0bf
Matthew Lamb <lamb.matthew@gmail.com>
2015-10-13 08:26:37 -0500

    Moved SpecialAbility.cs from excluded to included. VS derp.

---
+++ 5eCharGen/SpecialAbility.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace _5eCharGen
+{
+    public class SpecialAbility
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Dictionary<string, int> AttributeBonus { get; set; }
+
+        public SpecialAbility()
+        {
+            AttributeBonus = new Dictionary<string, int>();
+        }
+    }
+}
