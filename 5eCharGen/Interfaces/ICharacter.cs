602e472c704a982bb0c3421c262e1c0a82f08b2f
Travis Clark <clarktravism@gmail.com>
2015-10-15 23:41:44 -0500

    ICharacter Interface

---
+++ 5eCharGen/Interfaces/ICharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5eCharGen
+{
+    public interface ICharacter
+    {
+        string Name { get; set; }
+        int CharLevel { get; set; }
+        IList<CharClass> Classes { get; set; }
+        Race Race { get; set; }
+        IDictionary<string, int> Attributes { get; set; }
+        IList<Skills> Skills { get; set; }
+        IList<Skills> Feats { get; set; }
+        //Inventory Inventory { get; set; }
+    }
+}
