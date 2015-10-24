3ae5eb072d00a849dd29f8633cba8ad679e1add6
Matthew Lamb <lamb.matthew@gmail.com>
2015-10-24 18:13:34 -0500

    Two files somehow got excluded. Need to learn whatever shortcut that is I'm pressing by accident.

---
+++ 5eCharGen/Editor/View/ProficiencyEditor.cs
@@ -0,0 +1,111 @@
+﻿using _5eCharGen.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5eCharGen.Editor.View
+{
+    public partial class ProficiencyEditor : Form
+    {
+        public ProficiencyEditor()
+        {
+            InitializeComponent();
+            UpdateIndex();
+            comboBoxFieldProficiencySelect.ComboBoxTextChanged += 
+                new EventHandler(comboBoxFieldProficiencySelect_SelectedIndexChanged);
+        }
+
+        private void comboBoxFieldProficiencySelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxFieldProficiencySelect.ComboBox.SelectedItem != null)
+            {
+                string spellName = comboBoxFieldProficiencySelect.ComboBox.SelectedItem.ToString();
+                LoadProficiency(spellName);
+            }
+        }
+
+        private void LoadProficiency(string name)
+        {
+            Proficiency pf = Data.GetProficiency(name);
+            textFieldProfName.Text = pf.Name;
+            comboBoxProfType.ComboBox.SelectedIndex = (int) pf.Type;
+        }
+
+        private void UpdateIndex()
+        {
+            comboBoxProfType.ComboBox.Items.Clear();
+
+            for (ProfType pt = ProfType.Weapon; pt <= ProfType.Tool; pt++)
+            {
+                comboBoxProfType.ComboBox.Items.Add(pt.ToString());
+            }
+
+            comboBoxFieldProficiencySelect.ComboBox.Items.Clear();
+            foreach (Proficiency pf in Data.GetAllProficiencies().OrderBy(x => x.Name))
+            {
+                comboBoxFieldProficiencySelect.ComboBox.Items.Add(pf.Name);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Proficiency pf = new Proficiency();
+            pf.Name = textFieldProfName.Text;
+            pf.Type = (ProfType) comboBoxProfType.ComboBox.SelectedIndex;
+            try
+            {
+                Data.RemoveProficiency(pf.Name);
+            }
+            catch (KeyNotFoundException)
+            {
+                // If KeyNotFound we'll just add our new spell
+            }
+            finally
+            {
+                Data.AddProficiency(pf);
+
+            }
+            ClearForm();
+            UpdateIndex();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            string pf = comboBoxFieldProficiencySelect.ComboBox.SelectedItem.ToString();
+            if (pf.Length > 0)
+            {
+                DialogResult dr = MessageBox.Show(string.Format("Are you sure you want to remove the proficiency: {0}"
+                    , pf), "Confirm", MessageBoxButtons.YesNo);
+                switch (dr)
+                {
+                    case DialogResult.Yes:
+                        Data.RemoveProficiency(pf);
+                        goto default;
+                    default:
+                        ClearForm();
+                        UpdateIndex();
+                        break;
+                }
+            }
+        }
+
+        private void ClearForm()
+        {
+            textFieldProfName.Text = string.Empty;
+            UpdateIndex();
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+
+    }
+}
---
+++ 5eCharGen/Model/Proficiency.cs
@@ -0,0 +1,20 @@
+﻿using _5eCharGen.Model;
+
+namespace _5eCharGen
+{
+    public class Proficiency : IDataType
+    {
+        public string Name { get; set; }
+        public bool Learned { get; set; }
+        public ProfType Type { get; set; }
+        
+    }
+}
+
+namespace _5eCharGen.Model
+{
+    public enum ProfType
+    {
+        Weapon, Armor, Skill, Tool
+    }
+}
